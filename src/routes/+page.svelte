dad44228589466c117395a00084fb36dc817acc3
dioh0922 <diohound@gmail.com>
2025-12-20 09:19:36 +0900

    firebase db取得まで

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import {ref, get } from 'firebase/database'
+  import db from '$lib/firebase'
+
+  let beer: any | null = null
+  onMount(async () => {
+    const snapshot = await get(ref(db, 'beer'))
+    if(snapshot.exists()){
+      beer = snapshot.val()
+      console.log(snapshot.val())
+    }
+  })
+
+</script>
+
+
+<h3>{beer?.preference_type_definition?.summary}</h3>
+
+<ul>
+  <li>abv:{beer?.preferred_numeric_ranges?.abv}</li>
+  <li>ibu:{beer?.preferred_numeric_ranges?.ibu}</li>
+  <li>srm:{beer?.preferred_numeric_ranges?.srm}</li>
+  <li>type:{beer?.preferred_numeric_ranges?.finish_type}</li>
+
+</ul>
