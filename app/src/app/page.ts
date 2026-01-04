48637ad674c629ac338fd8b71bccacac798d6ba9
dioh0922 <diohound@gmail.com>
2026-01-04 10:11:20 +0900

    material追加

---
+++ app/src/app/page.ts
@@ -0,0 +1,20 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+@Component({
+  selector: 'app-root',
+  imports: [
+    RouterOutlet,
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatMenuModule
+  ],
+  templateUrl: './page.html',
+})
+export class Page {
+  protected readonly title = signal('app');
+}
