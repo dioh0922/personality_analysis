f2a064019c15d71429de490245bfb3252cccea93
dioh0922 <diohound@gmail.com>
2026-04-06 14:45:54 +0900

    会議結果保存

--- app/src/app/services/database-service.ts
+++ app/src/app/services/database-service.ts
@@ -1,6 +1,6 @@
 
 import { Injectable, NgZone, signal } from '@angular/core';
-import { Database, ref, onValue, set } from '@angular/fire/database';
+import { Database, ref, onValue, set, get, push } from '@angular/fire/database';
 
 @Injectable({
   providedIn: 'root',
@@ -27,5 +27,20 @@ export class DatabaseService {
   updateRankOrder = async (data: Array<any>) => {
     const rankDbRef = ref(this.db, '/beer/ranking');
     await set(rankDbRef, data);
+  }
+
+  saveDiscussion = async (data: any) => {
+    const now = new Date();
+
+    const timestampKey =
+    now.getFullYear().toString() +
+    String(now.getMonth() + 1).padStart(2, '0') +
+    String(now.getDate()).padStart(2, '0') +
+    String(now.getHours()).padStart(2, '0') +
+    String(now.getMinutes()).padStart(2, '0') +
+    String(now.getSeconds()).padStart(2, '0');
+
+    const discussionItemRef = ref(this.db, `/discussion/${timestampKey}`);
+    await set(discussionItemRef, data);
   }
 }
