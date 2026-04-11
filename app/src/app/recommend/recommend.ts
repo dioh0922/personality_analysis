c31bade84f59c68369cc6a48c40b0a9fd88b9fee
dioh0922 <diohound@gmail.com>
2026-04-11 09:15:59 +0900

    判定機能追加

--- app/src/app/recommend/recommend.ts
+++ app/src/app/recommend/recommend.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -8,7 +8,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSnackBarModule, MatSnackBar } from '@angular/material/snack-bar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-
+import { DatabaseService } from '../services/database-service';
 import axios from 'axios';
 
 @Component({
@@ -27,21 +27,31 @@ import axios from 'axios';
   templateUrl: './recommend.html',
   styleUrl: './recommend.css',
 })
-export class Recommend {
+export class Recommend implements OnInit {
   protected prompt: string = "";
   protected isLoading: boolean = false;
-  protected result: string = "";
+  protected result: {summary: string, agree: boolean} = {summary: "", agree: false};
+  protected profile: any = {};
+
   constructor(
+    private databaseService: DatabaseService,
     private snackBar: MatSnackBar
   ) {}
+
+  ngOnInit(): void {
+    this.databaseService.loadSake().then((data: any) => {
+      this.profile = data;
+    });
+  }
 
   submit = () => {
     this.isLoading = true;
     axios.post("https://kamata-portfolio.mydns.jp/extApi/aiSrc/recommend.php", {
         "prompt": this.prompt,
+        "profile": JSON.stringify(this.profile),
         "apiMode":true
     }).then((res: any) => {
-        this.result = res.data.result;
+        this.result = res.data;
     }).catch((er: any) => {
       this.snackBar.open(er.message, 'close');
     }).finally(() => {
