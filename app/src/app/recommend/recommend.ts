ed2c4070a8d31cf398963c78c034ff10342b9c53
dioh0922 <diohound@gmail.com>
2026-04-12 09:53:28 +0900

    判定区分を追加

--- app/src/app/recommend/recommend.ts
+++ app/src/app/recommend/recommend.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -6,6 +6,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTabsModule } from '@angular/material/tabs';
 import { MatSnackBarModule, MatSnackBar } from '@angular/material/snack-bar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { DatabaseService } from '../services/database-service';
@@ -21,15 +22,17 @@ import axios from 'axios';
     MatIconModule,
     MatSnackBarModule,
     MatProgressSpinnerModule,
+    MatTabsModule,
     CommonModule,
     FormsModule
   ],
   templateUrl: './recommend.html',
   styleUrl: './recommend.css',
 })
-export class Recommend implements OnInit {
+export class Recommend {
   protected prompt: string = "";
   protected isLoading: boolean = false;
+  protected selectedIndex: number = 0;
   protected result: {summary: string, agree: boolean} = {summary: "", agree: false};
   protected profile: any = {};
 
@@ -38,18 +41,14 @@ export class Recommend implements OnInit {
     private snackBar: MatSnackBar
   ) {}
 
-  ngOnInit(): void {
-    this.databaseService.loadSake().then((data: any) => {
-      this.profile = data;
-    });
-  }
-
-  submit = () => {
+  submit = async () => {
     this.isLoading = true;
+    await this.loadProfile();
+
     axios.post("https://kamata-portfolio.mydns.jp/extApi/aiSrc/recommend.php", {
         "prompt": this.prompt,
         "profile": JSON.stringify(this.profile),
-        "apiMode":true
+        "apiMode": true
     }).then((res: any) => {
         this.result = res.data;
     }).catch((er: any) => {
@@ -57,5 +56,26 @@ export class Recommend implements OnInit {
     }).finally(() => {
         this.isLoading = false;
     });
+  }
+
+  private loadProfile = (): Promise<void> => {
+    if(this.selectedIndex === 0) {
+      return this.databaseService.loadSake().then((data: any) => {
+        this.profile = data;
+      });
+    } else if(this.selectedIndex === 1) {
+      return this.databaseService.loadBeer().then((data: any) => {
+        const {ranking, taste_characteristics, tendency_graph, waveform, ...parsedData} = data;
+        this.profile = parsedData;
+      });
+    } else {
+      return Promise.all([
+        this.databaseService.loadSake(),
+        this.databaseService.loadBeer()
+      ]).then(([sakeData, beerData]) => {
+        const { ranking, taste_characteristics, tendency_graph, waveform, ...parsedBeerData } = beerData;
+        this.profile = { sake: sakeData, beer: parsedBeerData };
+      });
+    }
   }
 }
