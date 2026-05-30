6d06634d1de6b0e5bcb9b1ee4f3aafbeef2d92ed
dioh0922 <diohound@gmail.com>
2026-05-31 08:16:20 +0900

    コメント削除

--- app/src/app/sake/sake-review/review-list/review-list.ts
+++ app/src/app/sake/sake-review/review-list/review-list.ts
@@ -12,33 +12,4 @@ export class ReviewList {
   delete = (id: number) => {
     this.deleteEvent.emit(id)
   }
-  /*
-embedding_text
-:
-"水芭蕉 翠は、透明感、清涼感、華やかな香り、軽やかな甘みが理想に非常に近く、最も高評価となった。"
-id
-:
-1
-reason_summary
-:
-"理想に最も近い"
-retrieval_keywords
-:
-"[\"透明感\",\"清涼感\",\"華やか\",\"軽やか\"]"
-sake_name
-:
-"水芭蕉 翠"
-score
-:
-10
-sentiment
-:
-"best"
-tags
-:
-"[\"clean\",\"refreshing\",\"aromatic\",\"light_sweetness\",\"smooth\"]"
-type
-:
-"tasting_review"
-  */
 }
